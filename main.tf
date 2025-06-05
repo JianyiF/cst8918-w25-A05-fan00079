fcfbf5cfcf07eed2e0d119632e3c1c4f6d408842
JianyiF <fan00079@algonquinlive.com>
2025-06-05 19:02:09 -0400

    final mod

--- main.tf
+++ main.tf
@@ -24,3 +24,137 @@ provider "azurerm" {
 provider "cloudinit" {
   # Configuration options
 }
+
+variable "labelPrefix" {
+  description = "Prefix for resource naming"
+  type        = string
+}
+
+variable "region" {
+  default     = "Canada Central"
+  description = "Azure region"
+}
+
+variable "admin_username" {
+  default     = "azureadmin"
+}
+
+resource "azurerm_resource_group" "main" {
+  name     = "${var.labelPrefix}-A05-RG"
+  location = var.region
+}
+
+resource "azurerm_public_ip" "web" {
+  name                = "${var.labelPrefix}-ip"
+  location            = var.region
+  resource_group_name = azurerm_resource_group.main.name
+  allocation_method   = "Dynamic"
+}
+
+resource "azurerm_virtual_network" "main" {
+  name                = "${var.labelPrefix}-vnet"
+  address_space       = ["10.0.0.0/16"]
+  location            = var.region
+  resource_group_name = azurerm_resource_group.main.name
+}
+
+resource "azurerm_subnet" "web" {
+  name                 = "websubnet"
+  resource_group_name  = azurerm_resource_group.main.name
+  virtual_network_name = azurerm_virtual_network.main.name
+  address_prefixes     = ["10.0.1.0/24"]
+}
+
+resource "azurerm_network_security_group" "web" {
+  name                = "${var.labelPrefix}-nsg"
+  location            = var.region
+  resource_group_name = azurerm_resource_group.main.name
+
+  security_rule {
+    name                       = "Allow-SSH"
+    priority                   = 1001
+    direction                  = "Inbound"
+    access                     = "Allow"
+    protocol                   = "Tcp"
+    source_port_range          = "*"
+    destination_port_range     = "22"
+    source_address_prefix      = "*"
+    destination_address_prefix = "*"
+  }
+
+  security_rule {
+    name                       = "Allow-HTTP"
+    priority                   = 1002
+    direction                  = "Inbound"
+    access                     = "Allow"
+    protocol                   = "Tcp"
+    source_port_range          = "*"
+    destination_port_range     = "80"
+    source_address_prefix      = "*"
+    destination_address_prefix = "*"
+  }
+}
+
+resource "azurerm_network_interface" "web" {
+  name                = "${var.labelPrefix}-nic"
+  location            = var.region
+  resource_group_name = azurerm_resource_group.main.name
+
+  ip_configuration {
+    name                          = "webipconfig"
+    subnet_id                     = azurerm_subnet.web.id
+    private_ip_address_allocation = "Static"
+    public_ip_address_id          = azurerm_public_ip.web.id
+  }
+}
+
+resource "azurerm_network_interface_security_group_association" "web" {
+  network_interface_id      = azurerm_network_interface.web.id
+  network_security_group_id = azurerm_network_security_group.web.id
+}
+
+data "template_file" "init" {
+  template = file("${path.module}/init.sh")
+}
+
+resource "cloudinit_config" "web" {
+  part {
+    content_type = "text/x-shellscript"
+    content      = data.template_file.init.rendered
+  }
+}
+
+resource "azurerm_linux_virtual_machine" "web" {
+  name                = "${var.labelPrefix}-vm"
+  resource_group_name = azurerm_resource_group.main.name
+  location            = var.region
+  size                = "Standard_B1s"
+  admin_username      = var.admin_username
+  network_interface_ids = [
+    azurerm_network_interface.web.id
+  ]
+  admin_ssh_key {
+    username   = var.admin_username
+    public_key = file("/home/nealfan/.ssh/id_rsa.pub")
+  }
+  os_disk {
+    caching              = "ReadWrite"
+    storage_account_type = "Standard_LRS"
+    name                 = "${var.labelPrefix}-osdisk"
+  }
+  source_image_reference {
+    publisher = "Canonical"
+    offer     = "0001-com-ubuntu-server-jammy"
+    sku       = "22_04-lts"
+    version   = "latest"
+  }
+  custom_data = base64encode(cloudinit_config.web.rendered)
+}
+
+output "resource_group_name" {
+  value = azurerm_resource_group.main.name
+}
+
+output "public_ip_address" {
+  value = azurerm_public_ip.web.ip_address
+}
